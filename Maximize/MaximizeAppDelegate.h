679bb409f9bf4ebd0694d9e11198fc2937832ca4
Baris Metin <bmetin@verivue.com>
2012-02-29 13:52:20 -0500

    Initial Commit

---
+++ Maximize/MaximizeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MaximizeAppDelegate.h
+//  Maximize
+//
+//  Created by Baris Metin on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MaximizeAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Maximize/MaximizeAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  MaximizeAppDelegate.m
+//  Maximize
+//
+//  Created by Baris Metin on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MaximizeAppDelegate.h"
+
+@implementation MaximizeAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
